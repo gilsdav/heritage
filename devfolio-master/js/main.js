9d762125463f7a1b11558aae3328f5a933a74554
David Gilson <davgilson@live.fr>
2020-10-11 10:51:10 +0200

    update menu

--- devfolio-master/js/main.js
+++ devfolio-master/js/main.js
@@ -19,7 +19,7 @@
 	
 
 	$('#open-menu').click(function(){
-		initPDFViewer("/assets/menu.pdf", 10);
+		initPDFViewer("/assets/menu.pdf", 11);
 		$('#overlay').click(function(){
 			closePDFViewer();
 		});
